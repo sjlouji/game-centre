0e78b529dbeb95254c2a88da3e272dbae0585f99
sjlouji <38372696+sjlouji@users.noreply.github.com>
2025-10-19 03:02:10 +0530

    feat: Add Block Fall and Stack Game pages
    Introduces new pages for the Block Fall and Stack Game modes, rendering their respective screens.

---
+++ pages/block-fall.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import BlockFallScreen from '../screens/BlockFallScreen';
+
+const BlockFallPage: React.FC = () => {
+  return <BlockFallScreen />;
+};
+
+export default BlockFallPage;
---
+++ pages/stack.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import StackGameScreen from '../screens/StackGameScreen';
+
+const StackGamePage: React.FC = () => {
+  return <StackGameScreen />;
+};
+
+export default StackGamePage;
